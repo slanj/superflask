4cecff792dd8e4902f2477e864be7eee15f6460c
Slanj <slanjr@gmail.com>
2019-01-26 04:48:55 +0200

    user registration form added

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
-from myapp import app
-from myapp.forms import LoginForm
+from myapp import app, db
+from myapp.forms import LoginForm, RegistrationForm
 from myapp.models import User
 
 @app.route('/')
@@ -42,3 +42,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Horray! You are in our databse now!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register form', form=form)
