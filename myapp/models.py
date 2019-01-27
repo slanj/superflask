14114c28eefbfb9e0b20f77d824c715e962cd6b9
Slanj <slanjr@gmail.com>
2019-01-27 17:22:16 +0200

    user simple avater added

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from myapp import db, login
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
@@ -18,6 +19,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://gravatar.com/avatar/{}?s={}&d=identicon'.format(
+            digest, size)
 
 
 @login.user_loader
